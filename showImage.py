9662d0640021ceecfcd8205b2e14a59ef581f44a
musicLovezaki <zakky0727@gmail.com>
2023-12-17 19:56:17 +0900

    sessionではなくsq.liteを使ってデータを保存する方法に変更

--- showImage.py
+++ showImage.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.secret_key = 'zzzzz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # SQLiteデータベースを使用
+db = SQLAlchemy(app)
 
+class InputData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    input_number = db.Column(db.Integer)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -14,16 +19,19 @@ def index():
     # テンプレートに画像のパスを渡して表示
     return render_template('index.html',texts=generated_texts, image_path=image_path)
 
-@app.route('/simple_ver', methods=['GET'])
+@app.route('/simple_ver', methods=['GET', 'POST'])
 def simple_ver():
     image_path = 'static/output_graph_simple.png'
     generated_texts = ["-","dsds"]
     
     if request.method == 'POST':
-        input_number = int(request.form['input_number'])  # フォームからの数字を取得
-        session['input_number'] = input_number  # セッションにデータを保存
+        input_number = int(request.form['input_number'])
+        new_input = InputData(input_number=input_number)
+        db.session.add(new_input)
+        db.session.commit()
     else:
-        input_number = session.get('input_number', None)  # セッションからデータを取得
+        latest_input = InputData.query.order_by(InputData.id.desc()).first()
+        input_number = latest_input.input_number if latest_input else None
     
     return render_template('index.html',texts=generated_texts, image_path=image_path,input_number=input_number)
 
@@ -34,6 +42,10 @@ def Savitzky_Golay():
     generated_texts = ["-","dsds"]
     
     return render_template('index.html',texts=generated_texts,image_path=Savitzky_Golay_path)
+#if __name__ == '__main__':
+ #   db.create.all()
+  #  app.run(debug=True)
 
-if __name__ == '__main__':
+with app.app_context():
+    db.create_all()
     app.run(debug=True)
