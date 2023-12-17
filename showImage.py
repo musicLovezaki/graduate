9ebc3536d643919e2c96e671131c6128d08b79e0
musicLovezaki <zakky0727@gmail.com>
2023-12-17 12:53:14 +0900

    HTML上からWindowサイズを変更するための数字が入力できる場所を作成

--- showImage.py
+++ showImage.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import createPlot as CP
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     # 保存した画像のパス
     image_path = 'static/output_plot.png'
@@ -12,14 +12,18 @@ def index():
     # テンプレートに画像のパスを渡して表示
     return render_template('index.html',texts=generated_texts, image_path=image_path)
 
-@app.route('/simple_ver')
+@app.route('/simple_ver', methods=['GET'])
 def simple_ver():
-    simple_image_path = 'static/output_graph_simple.png'
-    generated_texts = "-"
-    
-    return render_template('index.html',texts=generated_texts, image_path=simple_image_path)
+    image_path = 'static/output_graph_simple.png'
+    generated_texts = ["-","dsds"]
 
-@app.route('/Savitzky-Golay')
+    if request.method == 'POST':
+        input_value = request.form['input_name']
+        print("Received input:", input_value)
+        
+    return render_template('index.html',texts=generated_texts, image_path=image_path)
+
+@app.route('/Savitzky-Golay', methods=['GET'])
 def Savitzky_Golay():
     Savitzky_Golay_path = 'static/output_graph_savitzky_golay.png'
     generated_texts = CP.texts
